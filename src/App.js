c06d8fe5091623645244d45b59a70dbb037c56c5
Brandon Clark <B.Y.Clark@dundee.ac.uk>
2024-01-10 01:20:30 +0000

    Added tests for api calls and cors complience

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+
+  const [data, setData] = useState(null);
+
+  useEffect(()=>{
+    fetch('http://192.168.1.41:5000/data', 
+    {method: 'GET',
+     mode : 'cors'})
+    .then(response => response.json())
+    .then(json => setData(json))
+    .catch(error => console.error(error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +30,10 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <div>
+          {data ? <pre>{JSON.stringify(data, null,2)}</pre> : 'Loading...'}
+        </div>
       </header>
     </div>
   );
